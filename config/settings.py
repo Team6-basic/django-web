28cd1495c70b2cfbc07be4874eb762211eb4a2be
JinhwanU <jhwan211@naver.com>
2023-06-20 20:31:13 +0900

    Chore set database

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*%7v9*zc)mj-5=yxi7wwzlm9+9*#m0w0iepxb-iek9sfd&m!29
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','hopecharm.net']
+ALLOWED_HOSTS = ['hopecharm.net']
 
 #로그인 로그아웃 성공 시 자동으로 이동할 URL
 LOGIN_REDIRECT_URL = '/'
@@ -79,9 +79,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",  # engine: mysql
+        "NAME": "testdb",  # DB Name
+        "USER": "admin",  # DB User
+        "PASSWORD": "Passw0rd",  # Password
+        "HOST": "arn:aws:rds:eu-west-3:940168446867:db:basic-rds-mariadb",  # 생성한 데이터베이스 엔드포인트
+        "PORT": "3306",  # 데이터베이스 포트
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
