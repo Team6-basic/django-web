bcc1aec9909a910b53de84984659a8453e600514
JinhwanU <jhwan211@naver.com>
2023-06-20 20:36:32 +0900

    Chore set database#2

--- config/settings.py
+++ config/settings.py
@@ -84,12 +84,11 @@ DATABASES = {
         "NAME": "testdb",  # DB Name
         "USER": "admin",  # DB User
         "PASSWORD": "Passw0rd",  # Password
-        "HOST": "arn:aws:rds:eu-west-3:940168446867:db:basic-rds-mariadb",  # 생성한 데이터베이스 엔드포인트
+        "HOST": "basic-rds-mariadb.chr1ojp8pl0h.eu-west-3.rds.amazonaws.com",  # 생성한 데이터베이스 엔드포인트
         "PORT": "3306",  # 데이터베이스 포트
         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
