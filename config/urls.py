f7e1376894056de9651b2172f6e1f1de8a4d0a15
JinhwanU <jhwan211@naver.com>
2023-06-23 12:03:36 +0000

    Recover#3

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hopecharm import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('common/', include('common.urls')),
-    path('', include('hopecharm.urls')), #hopecharm라는 주소로 입력이 들어오면, hopecharm 아래 urls.py로 보내겠다.
-    # path('',views.index, name='home'), #url에 아무것도 입력하지 않을 경우 404를 막기 위함.
+    # 로그인 권한제한
+    path('',auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('hopecharm/', include('hopecharm.urls')),
 ]
