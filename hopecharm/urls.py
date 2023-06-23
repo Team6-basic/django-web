4a40cb22ba84d959e1564594a7a2c9528368be1d
JinhwanU <jhwan211@naver.com>
2023-06-23 15:04:26 +0900

    Build : change directory#2

--- hopecharm/urls.py
+++ hopecharm/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+
+
 app_name = 'hopecharm'
 
 urlpatterns = [
-    path('',views.index, name='index'), #config/urls -> matjipmrk/urls -> matjipmrk/views.py 실행!
-    path('<int:board_id>/', views.detail, name='detail'),
+    path('',views.index, name='index'),
+    path('<int:board_id>/detail/', views.detail, name='detail'),
+    # path('hopecharm/<int:pk>/detail', ),
 ]
