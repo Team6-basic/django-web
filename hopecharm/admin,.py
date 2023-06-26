a389cbc26a008e8263b8ae9c468ad5601ff9a7cd
JinhwanU <jhwan211@naver.com>
2023-06-26 18:54:45 +0900

    Grant admin/models

--- hopecharm/admin,.py
+++ hopecharm/admin,.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from hopecharm.models import Day_Hate, Time_Hate
+from hopecharm.models import Day_Hate, Time_Hate, Device
 
 admin.site.register(Day_Hate)
 admin.site.register(Time_Hate)
+admin.site.register(Device)
