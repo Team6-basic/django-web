7a73c4049bd9144573289f068a8d0ae47a63f3ab
JinhwanU <jhwan211@naver.com>
2023-06-26 09:36:05 +0000

    merge

--- hopecharm/models.py
+++ hopecharm/models.py
@@ -1,29 +1,31 @@
 from django.db import models
-from django.db.models import Avg, Count, Min, Sum
-from django.conf import settings
+
+
+class Device(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    rp_serial = models.CharField(max_length=30)
+
 
 class Day_Hate(models.Model):
+    id = models.AutoField(primary_key=True)
     rec_date = models.DateField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_id = models.IntegerField(null=False)
 
-    def __str__(self):
-        return f"{self.rec_date}, {self.user}"
 
 class Time_Hate(models.Model):
-    create_time = models.DateTimeField(null=True)
-    label1 = models.FloatField(blank=True, null=True, verbose_name="여성/가족")
-    label2 = models.FloatField(blank=True, null=True, verbose_name="남성")
-    label3 = models.FloatField(blank=True, null=True, verbose_name="성소수자")
-    label4 = models.FloatField(blank=True, null=True, verbose_name="인종/국적")
-    label5 = models.FloatField(blank=True, null=True, verbose_name="연령")
-    label6 = models.FloatField(blank=True, null=True, verbose_name="지역")
-    label7 = models.FloatField(blank=True, null=True, verbose_name="종교")
-    label8 = models.FloatField(blank=True, null=True, verbose_name="기타혐오")
-    label9 = models.FloatField(blank=True, null=True, verbose_name="악플/욕설")
-    label10 = models.FloatField(blank=True, null=True, verbose_name="clean")
-
-    rec_day = models.ForeignKey(Day_Hate, on_delete=models.CASCADE)
-
-
-    def __str__(self):
-        return f"{self.rec_day}, {self.create_time}"
+    id = models.AutoField(primary_key=True)
+    rec_datetime = models.DateTimeField()
+    user_id = models.IntegerField(null=False)
+    fg_none = models.FloatField()
+    fg_other_hate = models.FloatField()
+    fg_man = models.FloatField()
+    fg_normal_bad_comments = models.FloatField()
+    fg_sexual_minority = models.FloatField()
+    fg_woman_or_family = models.FloatField()
+    fg_age = models.FloatField()
+    fg_race_or_nationality = models.FloatField()
+    fg_religion = models.FloatField()
+    fg_region = models.FloatField()
+    speech = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
