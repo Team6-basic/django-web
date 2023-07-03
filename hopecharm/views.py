4ed6106bf5af012dfe5df0d0ca93f20172b2cbf8
JinhwanU <jhwan211@naver.com>
2023-07-03 05:27:52 +0000

    Update front-end#3

--- hopecharm/views.py
+++ hopecharm/views.py
@@ -4,13 +4,6 @@ import numpy as np
 from django.core.paginator import Paginator
 from django.views import View
 from .models import Day_Hate, Time_Hate
-import datetime as dt
-
-# plotly 관련
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
 
 # Create your views here.
 def index(request):
@@ -48,137 +41,17 @@ def index(request):
                 }
 
     return render(request, 'hopecharm/main.html', context)
-
-
+    
 def detail(request, board_id):
-    user_data = Day_Hate.objects.filter(id=board_id).values('rec_date').get()
-    # 아래 board_df가 일별 총 DB테이블
-    board = Time_Hate.objects.filter(rec_datetime__date=user_data['rec_date'].isoformat()).values_list()
-    board_df = pd.DataFrame(board)
-    # 레이블용 Datetime 데이터!
-    time_labels = board_df[1]
+    day_hates = Day_Hate.objects.get(id=board_id)
+
+    board = Time_Hate.objects.filter(rec_day_id=board_id)
+    board_df = pd.DataFrame(list(board.values()))
     ### 여기다가 alert 이벤트 넣을거!
+    
 
     ### 여기다가 그래프 요소 넣을거!!
-    # 일벌 혐오표현 통계(파이차트) : 클린포함
-    # 결과 처리
-    datasets = []  # 딕셔너리 형태로 만들기
-    cols = board_df.columns[2:12]
-    for col in cols:
-        vals = board_df[col].sum()
-        datasets.append({col: vals})
 
-    # 월별 혐오표현 통계(라인차트) : 클린 제외
-    # 시계열그래프 그리기
-    fig = go.Figure()
-    board_df[1] = pd.to_datetime(board_df[1]).dt.tz_localize(None)
-
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_woman_or_family'],
-            name="여성/가족",
-            mode="lines",  # 라인으로 표현
-            # line=dict(color="#33CFA5") # 선색상 지정
-        )
-    )
-
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_man'],
-            name="남성",
-            mode="lines",  # 라인으로 표현
-        )
-    )
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_sexual_minority'],
-            name="성소수자",
-            mode="lines",  # 라인으로 표현
-        )
-    )
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_race_or_nationality'],
-            name="인종/국적",
-            mode="lines",  # 라인으로 표현
-        )
-    )
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_age'],
-            name="연령",
-            mode="lines",  # 라인으로 표현
-        )
-    )
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_region'],
-            name="지역",
-            mode="lines",  # 라인으로 표현
-        )
-    )
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_religion'],
-            name="종교",
-            mode="lines",  # 라인으로 표현
-        )
-    )
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_other_hate'],
-            name="기타혐오",
-            mode="lines",  # 라인으로 표현
-        )
-    )
-    fig.add_trace(
-        go.Scatter(  # 라인 그래프 그리기
-            x=board_df[1],
-            y=board_df['fg_normal_bad_comments'],
-            name="악플/욕설",
-            mode="lines",  # 라인으로 표현
-        )
-    )
-
-    fig.update_layout(
-        {
-            "title": {
-                "text": "<b>혐오표현 시계열그래프</b>",
-                "x": 0.5,  # x축 위치
-                "y": 0.9,
-                "font": {"size": 30},
-            },
-            "showlegend": True,  # 범례표시
-            "xaxis": {
-                "title": "date",  # x축 타이틀 이름
-                "showticklabels": True,  # x축 간격 표시
-                #'dtick' : 'M1' # x축 간격 범위
-            },
-            "yaxis": {"title": "빈도"},
-            "template": "plotly_white",  # 배경 설정
-        }
-    )
-
-    # HTML 파일로 변환
-    fig_timeline_html = fig.to_html()
-    # alert용 수치 높은 행만 추출하기!
-
-    return render(
-        request,
-        "hopecharm/detail.html",
-        {
-            "board": board_df,
-            "title": user_data,
-            "daily_list_pie": datasets,
-            "fig_timeline_html": fig_timeline_html,
-            "time_labels": time_labels,
-        },
-    )
+    
+    
+    return render(request, 'hopecharm/detail.html', {'board':board_df, 'dayhate':day_hates})
