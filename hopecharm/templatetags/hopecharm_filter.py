8f4ba3d0346f0323e108de6e5e7dbf9b3b08f7d4
JinhwanU <jhwan211@naver.com>
2023-07-03 06:28:16 +0000

    Update front-end#6

--- hopecharm/templatetags/hopecharm_filter.py
+++ hopecharm/templatetags/hopecharm_filter.py
@@ -1,14 +1,19 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
-
 
 @register.filter
 def sub(value, arg):
     return value - arg
 
-
-@register.filter("reportkey")
+@register.filter('reportkey')
 def reportkey(dict_data, key):
     if key in dict_data:
         return dict_data.get(key)
+    
+
+@register.filter
+def custom_filter(text, color):
+    safe_text = '<span style="color:{color}">{text}</span>'.format(color=color, text=text)
+    return mark_safe(safe_text)
